6264309ef78b2f76acead5f94820bc50818470eb
Lucas <lucasmonir@hotmail.com>
2024-11-16 11:40:34 -0300

    added gitignore and minor adjustments on economicDataService

--- InvestmentAPI/Services/Implementation/EconomicDataService.cs
+++ InvestmentAPI/Services/Implementation/EconomicDataService.cs
@@ -5,26 +5,28 @@ using Microsoft.Extensions.Options;
 
 namespace InvestmentAPI.Services.Implementation
 {
-    public class EconomicDataService : IEconomicDataService
+    public class EconomicDataService(HttpClient httpClient, IOptions<EconomicDataServiceConfiguration> config) : IEconomicDataService
 	{
-		private readonly HttpClient _httpClient;
-		private readonly EconomicDataServiceConfiguration _config;
-
-		public EconomicDataService(HttpClient httpClient, IOptions<EconomicDataServiceConfiguration> config)
-		{
-			_httpClient = httpClient;
-			_config = config.Value;
-		}
+		private readonly HttpClient _httpClient = httpClient;
+		private readonly EconomicDataServiceConfiguration _config = config.Value;
 
 		public async Task<string> GetInterest()
 		{
 			var range = DateTime.Now.AddDays(-1).ToString("dd/MM/yyyy");
-			var url = _config.BuildUrl("json", range, range);
-			var result = await _httpClient.GetStringAsync($"");
+			var format = "json";
+
+			var url = _config.BuildUrl(format, range, range);
+			var result = await _httpClient.GetStringAsync(url);
 
 			var json = JsonDocument.Parse(result);
+			string name = "valor";
 
-			return json.RootElement[0].GetProperty("valor").GetString() ?? "";
+			return GetJsonRootStringValue(name, json);
+		}
+		
+		private static string GetJsonRootStringValue(string name, JsonDocument json)
+		{
+			return json.RootElement[0].GetProperty(name).GetString() ?? "";
 		}
 	}
 }
